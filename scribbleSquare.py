80d2a63aa0020878d27ed164f306f6e6ccd8cd52
Gustaf Kylberg <gustaf.kylberg@vironova.com>
2019-07-08 08:55:52 +0200

    Update scribbleSquare.py
    Adding possibility to have reversed order of square sizes.

--- scribbleSquare.py
+++ scribbleSquare.py
@@ -10,19 +10,29 @@ img = skimage.data.load('P:/projects/AxiDraw/kylberg/wiggly_squares/pipe.jpg',as
 
 output_width = 20.0
 quantificationlevels = 7
+
+randomness_vertex = 0.0 # 0 is no randomness, 0.1 is suitable.
+randomness_position = 0.00 # 0.05
+
+min_pixel_size = 0.1
+max_pixel_size = 0.9
+
+small_first = True
+
 scale_factor = round(img.shape[0]/output_width)
+
 
 img = resize(img,(round(img.shape[0]/scale_factor), round(img.shape[1]/scale_factor)),anti_aliasing=True)
 img_orig = img
 img = 1-img
 img = np.round(np.multiply(img,quantificationlevels - 1)) + 1
 
-randomness_vertex = 0.1 # 0 is no randomness, 0.01 is suitable.
-randomness_position = 0.05
-min_pixel_size = 0.1
-max_pixel_size = 0.9
 def pixplot():
+        
     pixel_sizes = np.multiply(np.linspace(min_pixel_size,max_pixel_size,quantificationlevels),np.random.uniform(1-randomness_position,1+randomness_position,quantificationlevels))
+    if not small_first:
+        pixel_sizes = pixel_sizes[::-1]
+
     pixel_sizes = pixel_sizes[0:val]
     for n in pixel_sizes:
 
